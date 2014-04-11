bcfd542a2827b0e7a26622e7551912124366a206
Oceanswave <sean.mclellan@sixconcepts.com>
2014-04-11 16:02:59 -0400

    Lodash

---
+++ BaristaJS.AppEngine.Bundles.Lodash/LodashBundle.cs
@@ -0,0 +1,18 @@
+﻿namespace BaristaJS.AppEngine.Bundles.Lodash
+{
+  public class LodashBundle : IBundle
+  {
+    public IBundleMetadata Metadata
+    {
+      get;
+      set;
+    }
+
+    public object InstallBundle(IScriptEngine engine)
+    {
+      var lodash = Properties.Resources.lodash;
+
+      return engine.Evaluate(lodash);
+    }
+  }
+}
