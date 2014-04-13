c428041c84d564af6fdcefabd54c54fb570f0b7c
Oceanswave <sean.mclellan@sixconcepts.com>
2014-04-13 00:47:43 -0400

    Core Functionality

--- BaristaJS.AppEngine.Bundles.Core/BaristaInstance.cs
+++ BaristaJS.AppEngine.Bundles.Core/BaristaInstance.cs
@@ -1,102 +1,162 @@
 ﻿namespace BaristaJS.AppEngine.Bundles
 {
-    using BaristaJS.AppEngine;
-    using System;
-    using System.Collections.Generic;
+  using BaristaJS.AppEngine;
+  using System;
+  using System.Collections.Generic;
 
-    public class BaristaInstance : IScriptableObject
+  public class BaristaInstance : IScriptableObject
+  {
+    private IScriptEngine m_engine;
+
+    public BaristaInstance(IBundleManager bundleManager, string packageSource, IDictionary<string, object> env)
     {
-        private IScriptEngine m_engine;
+      if (bundleManager == null)
+        throw new ArgumentNullException("bundleManager");
 
-        public BaristaInstance(IBundleManager bundleManager, string packageSource, IDictionary<string, object> env)
-        {
-            if (bundleManager == null)
-                throw new ArgumentNullException("bundleManager");
+      if (String.IsNullOrWhiteSpace(packageSource))
+        throw new ArgumentNullException("packageSource");
 
-            if (String.IsNullOrWhiteSpace(packageSource))
-                throw new ArgumentNullException("packageSource");
+      if (env == null)
+        throw new ArgumentNullException("env");
 
-            if (env == null)
-                throw new ArgumentNullException("env");
-
-            BundleManager = bundleManager;
-            PackageSource = packageSource;
-            Environment = env;
-        }
-
-        #region Properties
-        
-        [NoScriptAccess]
-        public IBundleMetadata Metadata
-        {
-            get;
-            set;
-        }
-        
-        [NoScriptAccess]
-        public IBundleManager BundleManager
-        {
-            get;
-            private set;
-        }
-
-        [NoScriptAccess]
-        public IDictionary<string, object> Environment
-        {
-            get;
-            private set;
-        }
-
-        [NoScriptAccess]
-        public string PackageSource
-        {
-            get;
-            set;
-        }
-        
-        [ScriptMember(Name = "happyPuppy")]
-        public string HappyPuppy
-        {
-            get;
-            set;
-        }
-
-        [ScriptMember(Name = "version")]
-        public string Version
-        {
-            get { return m_baristaInstanceMetadata.Version.ToString(); }
-        }
-        #endregion
-
-        [ScriptMember("installedBundles")]
-        public IList<IBundle> InstalledBundles
-        {
-            get { return BundleManager.InstalledBundles; }
-        }
-        
-        [ScriptMember("require")]
-        public object Require(string packageId, string version = null)
-        {
-            var bundle = BundleManager.GetBundle<IBundle>(Environment, PackageSource, packageId, version);
-            return bundle.InstallBundle(m_engine);
-        }
-
-        [NoScriptAccess]
-        public void OnExposedToScriptCode(IScriptEngine engine)
-        {
-            if (engine == null)
-                throw new ArgumentNullException("engine");
-
-            if (m_engine == null)
-            {
-                m_engine = engine;
-                return;
-            }
-
-            if (engine != m_engine)
-            {
-                throw new ArgumentException("Invalid script engine", "engine");
-            }
-        }
+      BundleManager = bundleManager;
+      PackageSource = packageSource;
+      Environment = env;
     }
+
+    #region Properties
+
+    [NoScriptAccess]
+    public IBundleMetadata Metadata
+    {
+      get;
+      set;
+    }
+
+    [NoScriptAccess]
+    public IBundleManager BundleManager
+    {
+      get;
+      private set;
+    }
+
+    [NoScriptAccess]
+    public IDictionary<string, object> Environment
+    {
+      get;
+      private set;
+    }
+
+    [NoScriptAccess]
+    public string PackageSource
+    {
+      get;
+      set;
+    }
+
+    [ScriptMember(Name = "happyPuppy")]
+    public string HappyPuppy
+    {
+      get;
+      set;
+    }
+
+    [ScriptMember(Name = "version")]
+    public string Version
+    {
+      get { return this.Metadata.Version.ToString(); }
+    }
+    #endregion
+
+    [ScriptMember(Name = "isArray")]
+    public bool IsArray(object value)
+    {
+      return TypeUtilities.IsArray(value);
+    }
+
+    [ScriptMember(Name = "isDate")]
+    public bool IsDate(object value)
+    {
+      return TypeUtilities.IsDate(value);
+    }
+
+    [ScriptMember(Name = "isDefined")]
+    public bool IsDefined(object value)
+    {
+      return !TypeUtilities.IsUndefined(value);
+    }
+
+    [ScriptMember(Name = "isFunction")]
+    public bool IsFunction(object value)
+    {
+      return TypeUtilities.IsFunction(value);
+    }
+
+    [ScriptMember(Name = "isNumber")]
+    public bool IsNumber(object value)
+    {
+      return TypeUtilities.IsNumeric(value);
+    }
+
+    [ScriptMember(Name = "isObject")]
+    public bool IsObject(object value)
+    {
+      return TypeUtilities.IsObject(value);
+    }
+
+    [ScriptMember(Name = "isString")]
+    public bool IsString(object value)
+    {
+      return TypeUtilities.IsString(value);
+    }
+
+    [ScriptMember(Name = "isUndefined")]
+    public bool IsUndefined(object value)
+    {
+      return TypeUtilities.IsUndefined(value);
+    }
+
+    [ScriptMember(Name = "lowercase")]
+    public string Lowercase(object value)
+    {
+      return Convert.ToString(value).ToLowerInvariant();
+    }
+
+    [ScriptMember(Name = "uppercase")]
+    public string Uppercase(object value)
+    {
+      return Convert.ToString(value).ToUpperInvariant();
+    }
+
+    [ScriptMember("installedBundles")]
+    public IList<IBundle> InstalledBundles
+    {
+      get { return BundleManager.InstalledBundles; }
+    }
+
+    [ScriptMember("require")]
+    public object Require(string packageId, string version = null)
+    {
+      var bundle = BundleManager.GetBundle<IBundle>(Environment, PackageSource, packageId, version);
+      return bundle.InstallBundle(m_engine);
+    }
+
+    [NoScriptAccess]
+    public void OnExposedToScriptCode(IScriptEngine engine)
+    {
+      if (engine == null)
+        throw new ArgumentNullException("engine");
+
+      if (m_engine == null)
+      {
+        m_engine = engine;
+        return;
+      }
+
+      if (engine != m_engine)
+      {
+        throw new ArgumentException("Invalid script engine", "engine");
+      }
+    }
+  }
 }
---
+++ BaristaJS.AppEngine.Bundles.Core/TypeUtilities.cs
@@ -0,0 +1,87 @@
+﻿namespace BaristaJS.AppEngine.Bundles
+{
+  using System;
+
+  /// <summary>
+  /// Contains type-related functionality that isn't conversion or comparison.
+  /// </summary>
+  public static class TypeUtilities
+  {
+    /// <summary>
+    /// Returns <c>true</c> if the given value is undefined.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is undefined; <c>false</c> otherwise. </returns>
+    public static bool IsUndefined(object obj)
+    {
+      return obj == null || obj == Undefined.Value;
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given is an array.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is undefined; <c>false</c> otherwise. </returns>
+    public static bool IsArray(object obj)
+    {
+      //return obj is ArrayInstance;
+
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given value is a supported numeric type.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is a supported numeric type; <c>false</c>
+    /// otherwise. </returns>
+    public static bool IsNumeric(object obj)
+    {
+      return obj is double || obj is SByte || obj is Int16 || obj is Int32 || obj is Int64 || obj is byte || obj is UInt16 || obj is UInt32 || obj is UInt64;
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given value is a function.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is a function; <c>false</c>
+    /// otherwise. </returns>
+    public static bool IsFunction(object obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given value is a supported string type.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is a supported string type; <c>false</c>
+    /// otherwise. </returns>
+    public static bool IsString(object obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given value is a supported date type.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is a supported date type; <c>false</c>
+    /// otherwise. </returns>
+    public static bool IsDate(object obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the given value is an object.
+    /// </summary>
+    /// <param name="obj"> The object to check. </param>
+    /// <returns> <c>true</c> if the given value is an object; <c>false</c>
+    /// otherwise. </returns>
+    public static bool IsObject(object obj)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
