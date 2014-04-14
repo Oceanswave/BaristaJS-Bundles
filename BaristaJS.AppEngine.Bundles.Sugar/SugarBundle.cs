43901f7b586f92f568a94f71823b48b0dd2fc8e8
Sleepyhead <sean.mclellan@sixconcepts.com>
2014-04-14 08:39:32 -0400

    Added Sugar bundle.

---
+++ BaristaJS.AppEngine.Bundles.Sugar/SugarBundle.cs
@@ -0,0 +1,18 @@
+﻿namespace BaristaJS.AppEngine.Bundles.Sugar
+{
+    public class SugarBundle
+    {
+        public IBundleMetadata Metadata
+        {
+            get;
+            set;
+        }
+
+        public object InstallBundle(IScriptEngine engine)
+        {
+            var lodash = Properties.Resources.sugar_full_development;
+
+            return engine.Evaluate(lodash);
+        }
+    }
+}
